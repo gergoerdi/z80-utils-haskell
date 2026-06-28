4a79ee701b52a903a5210654a7f1b5b272692498
Gergo ERDI <gergo@erdi.hu>
2026-06-28 20:30:07 +0200

    [trs80] Add BASIC cassette exporter

--- src/Z80/Machine/TRS80/Cas.hs
+++ src/Z80/Machine/TRS80/Cas.hs
@@ -4,10 +4,40 @@ import Z80
 import Z80.Utils
 import qualified Data.ByteString as BS
 
+bcas :: BS.ByteString -> Z80ASM -> BS.ByteString
+bcas fileName asm = mconcat
+    [ leader
+    , wordBE 0x41fe
+    , wordBE end
+    , withCRC $ mconcat
+        [ word start
+        , bs
+        ]
+    ]
+  where
+    prog = org 0x4200 asm
+
+    bs = asmData prog
+    start = asmOrg prog
+    end = start + fromIntegral (BS.length bs)
+
+    wordBE w = BS.pack [hi, lo]
+      where
+        (lo, hi) = wordBytes w
+
+    word w = BS.pack [lo, hi]
+      where
+        (lo, hi) = wordBytes w
+
+leader :: BS.ByteString
+leader = BS.replicate 256 0x00 <> BS.singleton 0xa5
+
+withCRC :: BS.ByteString -> BS.ByteString
+withCRC bs = bs <> (BS.singleton . negate . BS.foldr' (+) 0 $ bs)
+
 cas :: BS.ByteString -> ASMBlock -> BS.ByteString
 cas fileName prog = mconcat
     [ leader
-    , BS.singleton 0xa5
     , BS.singleton 0x55
     , BS.take 6 . (<> BS.replicate 6 0x00) $ fileName
     , mconcat blocks
@@ -27,9 +57,6 @@ cas fileName prog = mconcat
             ]
         ]
     blocks = zipWith block [start, start + 256 ..] (splitInto 256 bs)
-
-    leader = BS.replicate 256 0x00
-    withCRC bs = bs <> (BS.singleton . negate . BS.foldr' (+) 0 $ bs)
 
     word w = BS.pack [lo, hi]
       where
